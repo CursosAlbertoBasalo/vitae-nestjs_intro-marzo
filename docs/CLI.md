7b170300b1fbb22ecd78cb5d57de7b3e32d74fa6
Alberto Basalo <albertobasalo@hotmail.com>
2022-03-15 18:14:43 +0100

    feat: controller base

--- docs/CLI.md
+++ docs/CLI.md
@@ -1,8 +1,12 @@
 ```bash
+# Generate app
+npx nest new my-app
+
 # Install nest
 npm i -g @nestjs/cli
-# Generate app
+ # Generate app
 nest new nest-intro -p npm
+
 # Run in dev mode
 npm run start:dev
 
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private appService: AppService) {}
 
-  @Get()
+  @Get('')
   getHello(): string {
+    // return 'Hola Vitae';
+    // return new Vitae().saludar();
     return this.appService.getHello();
   }
+
+  @Get('/test')
+  getTest(): string {
+    return 'Hola Test';
+  }
+
+  @Get('/param/:id')
+  getParam(@Param('id') id: string): string {
+    const type = typeof id;
+    return `Param: ${id} of type ${type}`;
+  }
+
+  @Get('/square/:someParam')
+  getSquare(@Param('someParam') someParam: number): string {
+    const type = typeof someParam;
+    const square = someParam * someParam;
+    return `Square of ${someParam} of type ${type} is ${square}`;
+  }
+
+  @Get('/square/pipe/:someNumber')
+  getSquarePipe(@Param('someNumber', ParseIntPipe) someNumber: number): string {
+    const type = typeof someNumber;
+    const square = someNumber * someNumber;
+    return `Square of ${someNumber} of type ${type} is ${square}`;
+  }
 }
+
+// class Vitae {
+//   private nombre = 'Vitae';
+//   //private numero: number;
+
+//   constructor(private numero: number) {
+//     //this.numero = numero;
+//     console.log('Constructor Vitae', numero);
+//   }
+
+//   saludar() {
+//     console.log('Saludando', this.numero);
+//     return 'Hola ' + this.nombre;
+//   }
+//}
