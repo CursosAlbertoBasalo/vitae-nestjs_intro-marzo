58ea9dd05c4cc269daa060c4ba42e72d0b08d1df
Alberto Basalo <albertobasalo@hotmail.com>
2022-03-17 19:11:09 +0100

    feat: jwt

--- src/auth/jwt-strategy.service.ts
+++ src/auth/jwt-strategy.service.ts
@@ -7,7 +7,7 @@ export class JwtStrategyService extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
+      ignoreExpiration: true,
       secretOrKey: 'secret',
     });
   }
