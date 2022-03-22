65d7b7d389b4162a8ff4d65369c395847c0eb478
Alberto Basalo <albertobasalo@hotmail.com>
2022-03-22 17:35:50 +0100

    fix: guid

--- src/auth/auth-mongo.service.ts
+++ src/auth/auth-mongo.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { UtilsService } from 'src/utils/utils.service';
 import { CredentialsDto } from './dto/credentials.dto';
 import { LoginDto } from './dto/login.dto';
 import { RegistrationDto } from './dto/registration.dto';
@@ -11,19 +12,21 @@ import { User } from './entities/user.entity';
 export class AuthMongoService {
   constructor(
     private readonly jwtService: JwtService,
+    private readonly utilsService: UtilsService,
     @InjectModel(User.name) private readonly userModel: Model<User>,
   ) {}
 
   async register(registration: RegistrationDto): Promise<CredentialsDto> {
     const user = await this.userModel.create({
       ...registration,
-      id: '',
+      id: this.utilsService.createGUID(),
       createdAt: new Date(),
       updatedAt: new Date(),
     });
     await user.save();
     return this.buildCredentials(user);
   }
+
   async login(login: LoginDto): Promise<CredentialsDto> {
     const user = await this.userModel.create();
     return undefined;
