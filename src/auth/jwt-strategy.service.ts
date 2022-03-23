c3a0aa8175de2c2a476b31cc41861940473d5299
Alberto Basalo <albertobasalo@hotmail.com>
2022-03-23 17:55:25 +0100

    feat: twt mongo

--- src/auth/jwt-strategy.service.ts
+++ src/auth/jwt-strategy.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthMongoService } from './auth-mongo.service';
 
 @Injectable()
 export class JwtStrategyService extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly authService: AuthMongoService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: true,
@@ -13,7 +14,10 @@ export class JwtStrategyService extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    // req.user = payload;
-    return { userId: payload.sub, roles: ['admin'] };
+    const userId = payload.sub;
+    const user = await this.authService.findById(userId);
+    // appended to req.user property
+    return user;
+    // return { userId: payload.sub };
   }
 }
