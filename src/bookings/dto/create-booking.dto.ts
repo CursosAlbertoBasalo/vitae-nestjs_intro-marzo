28f212da710d9e6d450d6c6ce398cde63b8fb907
Alberto Basalo <albertobasalo@hotmail.com>
2022-03-23 18:38:05 +0100

    docs: bookings

--- src/bookings/dto/create-booking.dto.ts
+++ src/bookings/dto/create-booking.dto.ts
@@ -1,1 +1,13 @@
-export class CreateBookingDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateBookingDto {
+  @IsString()
+  @IsNotEmpty()
+  client: string;
+
+  @IsNumber()
+  passengers: number;
+
+  @IsString()
+  tripId: string;
+}
